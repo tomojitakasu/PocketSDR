215f0bdf5a5d3318210acfb05c63c9d1f50753b5
tomojitakasu <ttaka@yk.rim.or.jp>
2022-01-20 13:51:31 +0900

    Added.

---
+++ test/pocket_trk_prof.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+#  profiling pocket_trk.py
+#
+
+python -m cProfile -s cumtime ../python/pocket_trk.py L1.bin -f 12 -fi 3 -sig L1CA -prn 193,194,196 -q > prof1.txt
+
+python -m cProfile -s cumtime ../python/pocket_trk.py L6.bin -f 12        -sig L6D -prn 193,194,196 -q > prof2.txt
+
