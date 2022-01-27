0e796807ac65e27fb476ca96d6d8fa8d8a5d7a33
tomojitakasu <ttaka@yk.rim.or.jp>
2022-01-27 18:02:00 +0900

    Added.

---
+++ lib/build/libfec.mk
@@ -0,0 +1,33 @@
+#
+# makefile of LIBFEC shared library (libfec.so)
+#
+#! You need to install LIBFEC source tree as follows.
+#!
+#! $ git clone https://github.com/quiet/libfec libfec
+
+#! specify directory of LIBFEC source tree
+SRC = ./libfec
+
+#! uncomment for Windows
+INSTALL = ../win32
+
+#! uncomment for Linuex
+#INSTALL = ../linux
+
+TARGET = libfec.so
+
+$(TARGET) :
+	DIR=`pwd`; \
+	cd $(SRC); \
+	./configure; \
+	sed 's/-lc//' < makefile > makefile.p; \
+	mv makefile.p makefile; \
+	make; \
+	cd $$DIR; \
+	cp $(SRC)/$(TARGET) $(TARGET)
+
+clean:
+	rm -f $(TARGET)
+
+install:
+	cp $(TARGET) $(INSTALL)
