8adce7b3910d95880d869976332a85b43407806f
tomojitakasu <ttaka@yk.rim.or.jp>
2022-02-15 18:32:27 +0900

    Add G3OC test.

--- test/pocket_acq_test.sh
+++ test/pocket_acq_test.sh
@@ -3,12 +3,12 @@
 #  test driver for pocket_acq.py
 #
 
-../bin/pocket_dump -t 10 L1.bin L2.bin -c ../conf/pocket_L1L2_8MHz.conf
+../bin/pocket_dump -t 10 L1.bin L2.bin -c ../conf/pocket_L1L2_4MHz.conf
 
-../python/pocket_acq.py L1.bin -f 8 -fi 2 -np -sig L1CA -prn 1-32,193-199
-../python/pocket_acq.py L1.bin -f 8 -fi 2 -np -sig L1CA -prn 120-158
-../python/pocket_acq.py L1.bin -f 8 -fi 2 -np -sig L1S  -prn 184-191
-../python/pocket_acq.py L2.bin -f 8 -fi 2 -np -sig L2CM -prn 1-32,193-199
+../python/pocket_acq.py L1.bin -f 4 -np -sig L1CA -prn 1-32,193-199
+../python/pocket_acq.py L1.bin -f 4 -np -sig L1CA -prn 120-158
+../python/pocket_acq.py L1.bin -f 4 -np -sig L1S  -prn 184-191
+../python/pocket_acq.py L2.bin -f 4 -np -sig L2CM -prn 1-32,193-199
 
 ../bin/pocket_dump -t 10 L1a.bin L5.bin -c ../conf/pocket_L1L5_24MHz.conf
 
@@ -16,8 +16,8 @@
 ../python/pocket_acq.py L1a.bin -f 24 -fi 6 -np -sig L1CD -prn 1-32,193-199
 ../python/pocket_acq.py L1a.bin -f 24 -fi 6 -np -sig E1B  -prn 1-36
 ../python/pocket_acq.py L1a.bin -f 24 -fi 6 -np -sig E1C  -prn 1-36
-../python/pocket_acq.py L1a.bin -f 24 -fi 6 -np -sig B1CP -prn 1-63
-../python/pocket_acq.py L1a.bin -f 24 -fi 6 -np -sig B1CD -prn 1-63
+../python/pocket_acq.py L1a.bin -f 24 -fi 6 -np -sig B1CP -prn 19-46
+../python/pocket_acq.py L1a.bin -f 24 -fi 6 -np -sig B1CD -prn 19-46
 
 ../python/pocket_acq.py L5.bin -f 24 -np -sig L5I  -prn 1-32,193-199
 ../python/pocket_acq.py L5.bin -f 24 -np -sig L5Q  -prn 1-32,193-199
@@ -27,8 +27,8 @@
 ../python/pocket_acq.py L5.bin -f 24 -np -sig L5SQ -prn 184-189
 ../python/pocket_acq.py L5.bin -f 24 -np -sig E5AI -prn 1-36
 ../python/pocket_acq.py L5.bin -f 24 -np -sig E5AQ -prn 1-36
-../python/pocket_acq.py L5.bin -f 24 -np -sig B2AP -prn 1-63
-../python/pocket_acq.py L5.bin -f 24 -np -sig B2AD -prn 1-63
+../python/pocket_acq.py L5.bin -f 24 -np -sig B2AP -prn 19-46
+../python/pocket_acq.py L5.bin -f 24 -np -sig B2AD -prn 19-46
 ../python/pocket_acq.py L5.bin -f 24 -np -sig I5S  -prn 1-7
 
 ../bin/pocket_dump -t 10 L1.bin L6.bin -c ../conf/pocket_L1L6_12MHz.conf
@@ -43,13 +43,19 @@
 ../python/pocket_acq.py G1.bin -f 12 -np -sig G1CA -prn -7-6
 ../python/pocket_acq.py G2.bin -f 12 -np -sig G2CA -prn -7-6
 
-../bin/pocket_dump -t 10 E1.bin E5B.bin -c ../conf/pocket_E1E5B_24MHz.conf
+../bin/pocket_dump -t 10 G1a.bin G3.bin -c ../conf/pocket_G1G3_24MHz.conf
+
+../python/pocket_acq.py G1a.bin -f 24 -np -sig G1CA  -prn -7-6
+../python/pocket_acq.py G3.bin  -f 24 -np -sig G3OCD -prn 1-24
+../python/pocket_acq.py G3.bin  -f 24 -np -sig G3OCP -prn 1-24
+
+../bin/pocket_dump -t 10 E1.bin E5b.bin -c ../conf/pocket_E1E5b_24MHz.conf
 
 ../python/pocket_acq.py E1.bin  -f 24 -np -sig E1B  -prn 1-36
 ../python/pocket_acq.py E1.bin  -f 24 -np -sig E1C  -prn 1-36
-../python/pocket_acq.py E5B.bin -f 24 -np -sig E5BI -prn 1-36
-../python/pocket_acq.py E5B.bin -f 24 -np -sig E5BQ -prn 1-36
-../python/pocket_acq.py E5B.bin -f 24 -np -sig B2BI -prn 1-63
+../python/pocket_acq.py E5b.bin -f 24 -np -sig E5BI -prn 1-36
+../python/pocket_acq.py E5b.bin -f 24 -np -sig E5BQ -prn 1-36
+../python/pocket_acq.py E5b.bin -f 24 -np -sig B2BI -prn 19-46
 
 ../bin/pocket_dump -t 10 B1I.bin B2I.bin -c ../conf/pocket_B1IB2I_12MHz.conf
 
@@ -58,6 +64,6 @@
 
 ../bin/pocket_dump -t 10 B1Ia.bin B3I.bin -c ../conf/pocket_B1IB3I_24MHz.conf
 
-../python/pocket_acq.py B1Ia.bin -f 24 -np -sig B1I -prn 1-37
-../python/pocket_acq.py B3I.bin -f 24 -np -sig B3I -prn 1-63
+../python/pocket_acq.py B1Ia.bin -f 24 -np -sig B1I -prn 1-63
+../python/pocket_acq.py B3I.bin  -f 24 -np -sig B3I -prn 1-63
 
