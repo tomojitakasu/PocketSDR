bc407cfa15facd0edeacc940251776bec8612c70
tomojitakasu <ttaka@yk.rim.or.jp>
2022-05-17 22:24:32 +0900

    updated.

--- lib/build/libsdr.mk
+++ lib/build/libsdr.mk
@@ -20,25 +20,33 @@ INCLUDE = -I$(SRC)
 #! comment out for older CPU without AVX2
 OPTIONS = -DAVX2
 
-CFLAGS = -Ofast -march=native $(INCLUDE) $(OPTIONS) -fPIC -g
+CFLAGS = -Ofast -march=native $(INCLUDE) $(OPTIONS) -Wall -fPIC -g
 
 LDLIBS = -lfftw3f
 
-OBJ = sdr_func.o sdr_cmn.o
+OBJ = sdr_cmn.o sdr_func.o sdr_code.o sdr_code_gal.o
 
 TARGET = libsdr.so
 
 $(TARGET) : $(OBJ)
 	$(CC) -shared -o $@ $(OBJ) $(LDLIBS)
 
-sdr_func.o : $(SRC)/sdr_func.c
-	$(CC) -c $(CFLAGS) $(SRC)/sdr_func.c
-
 sdr_cmn.o : $(SRC)/sdr_cmn.c
 	$(CC) -c $(CFLAGS) $(SRC)/sdr_cmn.c
 
-sdr_func.o : $(SRC)/pocket.h
+sdr_func.o : $(SRC)/sdr_func.c
+	$(CC) -c $(CFLAGS) $(SRC)/sdr_func.c
+
+sdr_code.o : $(SRC)/sdr_code.c
+	$(CC) -c $(CFLAGS) $(SRC)/sdr_code.c
+
+sdr_code_gal.o : $(SRC)/sdr_code_gal.c
+	$(CC) -c $(CFLAGS) $(SRC)/sdr_code_gal.c
+
 sdr_cmn.o  : $(SRC)/pocket.h
+sdr_func.o : $(SRC)/pocket.h
+sdr_code.o : $(SRC)/pocket.h
+sdr_code_gal.o : $(SRC)/pocket.h
 
 clean:
 	rm -f $(TARGET) *.o
