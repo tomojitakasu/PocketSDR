0fb644bce8cb45479b9fe956d74f2bfd9c724a85
tomojitakasu <ttaka@yk.rim.or.jp>
2022-07-08 21:24:58 +0900

    Add option -v.

--- python/pocket_snap.py
+++ python/pocket_snap.py
@@ -7,6 +7,7 @@
 #
 #  History:
 #  2022-02-14  1.0  new
+#  2022-07-08  1.1  add option -v
 #
 import sys, math, time, re
 import numpy as np
@@ -18,6 +19,7 @@ THRES_CN0 = 37.0    # threshold to lock signal (dB-Hz)
 EL_MASK   = 15.0    # elevation mask (deg)
 MAX_DOP   = 5000.0  # max Doppler freq. to search signal (Hz)
 MAX_DFREQ = 500.0   # max freq. offset of ref oscillator (Hz)
+VERP      = 0       # verpose display flag
 
 # global variables -------------------------------------------------------------
 code_fft = {}       # code FFT caches
@@ -25,7 +27,7 @@ code_fft = {}       # code FFT caches
 # show usage -------------------------------------------------------------------
 def show_usage():
     print('Usage: pocket_snap.py [-ts time] [-pos lat,lon,hgt] [-ti sec] [-toff toff]')
-    print('       [-f freq] [-fi freq] [-tint tint] [-sys sys[,...]] -nav file')
+    print('       [-f freq] [-fi freq] [-tint tint] [-sys sys[,...]] [-v] -nav file')
     print('       [-out file] file')
     exit()
 
@@ -96,12 +98,15 @@ def search_sig(data, sig, sys, prn, dif, fs, fi, rrate):
         dop = sdr_func.fine_dop(P.T[ix[1]], fds, ix[0])
         rrate = -dop * CLIGHT / sdr_code.sig_freq(sig)
         coff = fine_coff(sig, fs, P[ix[0]], np.arange(0, T, 1.0 / fs), ix[1])
-        #print('%s : SIG=%-5s C/N0=%5.1f dB-Hz DOP=%9.3f m/s COFF=%12.9f ms' %
-        #    (satno2id(sat), sig, cn0, dop, coff * 1e3))
+        if VERP:
+            print('%s : SIG=%-5s C/N0=%5.1f dB-Hz DOP=%9.3f Hz COFF=%12.9f ms' %
+                (satno2id(sat), sig, cn0, dop, coff * 1e3))
         data.append([sat, rrate, coff])
 
 # search signals ---------------------------------------------------------------
 def search_sigs(time, ssys, dif, fs, fi, rr, nav):
+    if VERP:
+        print('search_sigs')
     data = []
     if ssys & SYS_GPS:
         for prn in range(1, 33):
@@ -138,6 +143,8 @@ def drdot_dx(rs, vs, x):
 
 # position by Doppler ----------------------------------------------------------
 def pos_dop(data, spos):
+    if VERP:
+        print('pos_dop')
     N = len(data)
     x = np.zeros(4)
     v = np.zeros(N)
@@ -154,14 +161,17 @@ def pos_dop(data, spos):
         if n < 4:
             return np.zeros(3)
         dx, res, rank, s = np.linalg.lstsq(H[:n,:], v[:n], rcond=None)
+        if VERP:
+            print('(%d) N=%2d  POS=%s  RES=%10.3f m/s' % (i, n, pos_str(x), norm(v)))
         x += dx
-        #print('(%d) N=%2d  POS=%s  RES=%10.3f' % (i, n, pos_str(x), norm(v)))
         if norm(dx) < 1.0:
             return x[:3]
     return np.zeros(3)
 
 # resolve ms ambiguity in code offset -----------------------------------------
 def res_coff_amb(data, spos, rr):
+    if VERP:
+        print('res_coff_amb')
     N = len(data)
     tau = np.ones(N) * 1e9
     for i in range(N):
@@ -175,9 +185,14 @@ def res_coff_amb(data, spos, rr):
         if norm(spos[i][0]) > 1e-3:
             off = (tau[i] - tau_ref) - (data[i][2] - coff_ref)
             data[i][2] += np.round(off * 1e3) * 1e-3 
+            if VERP:
+                print('%s - %s: N=%8.5f -> %8.5f' % (satno2id(data[i][0]),
+                    satno2id(data[idx][0]), off * 1e3, np.round(off * 1e3)))
     
 # estimate position by code offsets ------------------------------------------
 def pos_coff(time, data, rr, nav):
+    if VERP:
+        print('pos_coff')
     N = len(data)
     x = np.zeros(5)
     x[:3] = rr
@@ -201,9 +216,10 @@ def pos_coff(time, data, rr, nav):
         if n < 5:
             break
         dx, res, rank, s = np.linalg.lstsq(H[:n,:], v[:n], rcond=None)
+        if VERP:
+            print('(%d) N=%2d  POS=%s  CLK=%9.6f  DT=%9.6f  RES=%10.3f m' % (i,
+                n, pos_str(x), x[3] / CLIGHT, x[4], np.sqrt(np.dot(v, v) / n)))
         x += dx
-        #print('(%d) N=%2d  POS=%s  CLK=%9.6f %9.6f  RES=%10.3f' % (i, n,
-        #    pos_str(x), x[3] / CLIGHT, x[4], np.sqrt(np.dot(v, v) / n)))
         if norm(dx) < 1e-3:
             if np.sqrt(np.dot(v, v) / n) > 1e3:
                 break
@@ -283,6 +299,9 @@ def path_time(file):
 #     -sys sys[,...]
 #         Select navigation system(s) (G=GPS,E=Galileo,J=QZSS,C=BDS). [G]
 #
+#     -v
+#         Enable verpose status display.
+#
 #     -nav file
 #         RINEX navigation data file.
 #
@@ -334,6 +353,8 @@ if __name__ == '__main__':
         elif sys.argv[i] == '-out':
             i += 1
             ofile = sys.argv[i]
+        elif sys.argv[i] == '-v':
+            VERP = 1
         elif sys.argv[i][0] == '-':
             show_usage()
         else:
