794bc9772ec6be88adaf58fda57717cd733a0a73
Tomoji Takasu <ttaka@yk.rim.or.jp>
2023-12-24 22:22:26 +0900

    Support max 8 CH inputs, DATA_CYC: 50 -> 10 (ms).

--- app/pocket_dump/pocket_dump.c
+++ app/pocket_dump/pocket_dump.c
@@ -10,15 +10,18 @@
 //  2022-01-04  1.1  data capture cycle: 1 -> 50 (ms)
 //  2022-01-06  1.2  add output to stdout with "-" as file path
 //                   add option -q to suppress output of status
+//  2022-08-31  1.3  support max 8 CH inputs
+//                   DATA_CYC: 50 -> 10 (ms)
 //
 #include <signal.h>
 #include <time.h>
 #include "pocket_dev.h"
 
 // constants and macros --------------------------------------------------------
-#define PROG_NAME       "pocket_dump" /* program name */
-#define DATA_CYC        50      /* data capture cycle (ms) */
-#define RATE_CYC        1000    /* data rate update cycle (ms) */
+#define PROG_NAME       "pocket_dump" // program name
+#define DATA_CYC        10      // data capture cycle (ms)
+#define STAT_CYC        50      // status update cycle (ms)
+#define RATE_CYC        1000    // data rate update cycle (ms)
 
 // interrupt flag --------------------------------------------------------------
 static volatile uint8_t intr = 0;
@@ -58,43 +61,46 @@ static void sig_func(int sig)
 // print usage -----------------------------------------------------------------
 static void print_usage(void)
 {
-    printf("Usage: %s [-t tsec] [-r] [-p bus[,port]] [-c conf_file] [-q] [file [file]]\n",
-        PROG_NAME);
+    printf("Usage: %s [-t tsec] [-r] [-p bus[,port]] [-c conf_file] [-q]\n"
+        "    [file [file ...]]\n", PROG_NAME);
     exit(0);
 }
 
 // dump digital IF data --------------------------------------------------------
-static void dump_data(int bus, int port, double tsec, int raw, int quiet,
+static void dump_data(sdr_dev_t *dev, double tsec, int raw, int quiet,
     FILE **fp)
 {
-    static const char *str_IQ[] = {"-", "I", "IQ"};
-    sdr_dev_t *dev;
+    static const char *str_IQ[] = {"-", "I", "IQ", "I"};
     double time = 0.0, time_p = 0.0, sample = 0.0, sample_p = 0.0;
     double rate = 0.0, byte[SDR_MAX_CH] = {0};
     int8_t *buff[SDR_MAX_CH];
     int size, n[SDR_MAX_CH];
     
-    if (!(dev = sdr_dev_open(bus, port))) {
-        return;
-    }
     if (raw) {
-        dev->IQ[0] = dev->IQ[1] = 0;
+        for (int i = 0; i < dev->max_ch; i++) {
+            dev->IQ[i] = 0;
+        }
     }
-    for (int i = 0; i < SDR_MAX_CH; i++) {
+    for (int i = 0; i < dev->max_ch; i++) {
         size = SDR_SIZE_BUFF * SDR_MAX_BUFF * (dev->IQ[i] == 2 ? 2 : 1);
         buff[i] = (int8_t *)malloc(size);
     }
     if (!quiet) {
-        fprintf(stderr, "%9s  %3s %12s %3s %12s %12s\n", "TIME(s)", "T",
-            "CH1(Bytes)", "T", "CH2(Bytes)", "RATE(Ks/s)");
+        fprintf(stderr, "%9s ", "TIME(s)");
+        for (int i = 0; i < dev->max_ch; i++) {
+            if (!fp[i]) continue;
+            fprintf(stderr, " %3s   CH%d(Bytes)", "T", i + 1);
+        }
+        fprintf(stderr, " %12s\n", "RATE(Ks/s)");
     }
     uint32_t tick = get_tick();
     
     for (int i = 0; !intr && (tsec <= 0.0 || time < tsec); i++) {
-        time = (get_tick() - tick) * 1e-3;
-        
+        if (!quiet) {
+            time = (get_tick() - tick) * 1e-3;
+        }
         if ((size = sdr_dev_data(dev, buff, n)) > 0) {
-            for (int j = 0; j < SDR_MAX_CH; j++) {
+            for (int j = 0; j < dev->max_ch; j++) {
                 if (!fp[j]) continue;
                 fwrite(buff[j], n[j], 1, fp[j]);
                 fflush(fp[j]);
@@ -102,44 +108,48 @@ static void dump_data(int bus, int port, double tsec, int raw, int quiet,
             }
             sample += size;
         }
-        if (time - time_p > RATE_CYC * 1e-3) {
+        if (!quiet && time - time_p > RATE_CYC * 1e-3) {
             rate = (sample - sample_p) / (time - time_p);
             time_p = time;
             sample_p = sample;
         }
-        if (!quiet) {
-            fprintf(stderr, "%9.1f  %3s %12.0f %3s %12.0f %12.1f\r", time,
-                str_IQ[dev->IQ[0]], byte[0], str_IQ[dev->IQ[1]], byte[1],
-                rate * 1e-3);
+        if (!quiet && i % (STAT_CYC / DATA_CYC) == 0) {
+            fprintf(stderr, "%9.1f ", time);
+            for (int j = 0; j < dev->max_ch; j++) {
+                if (!fp[j]) continue;
+                fprintf(stderr, " %3s %12.0f", str_IQ[dev->IQ[j]], byte[j]);
+            }
+            fprintf(stderr, " %12.1f\r", rate * 1e-3);
             fflush(stderr);
         }
         sleep_msec(DATA_CYC);
     }
     if (!quiet) {
         rate = time > 0.0 ? sample / time : 0.0;
-        fprintf(stderr, "%9.1f  %3s %12.0f %3s %12.0f %12.1f\n", time,
-            str_IQ[dev->IQ[0]], byte[0], str_IQ[dev->IQ[1]], byte[1],
-            rate * 1e-3);
+        fprintf(stderr, "%9.1f ", time);
+        for (int j = 0; j < dev->max_ch; j++) {
+            if (!fp[j]) continue;
+            fprintf(stderr, " %3s %12.0f", str_IQ[dev->IQ[j]], byte[j]);
+        }
+        fprintf(stderr, " %12.1f\n", rate * 1e-3);
     }
-    for (int i = 0; i < SDR_MAX_CH; i++) {
+    for (int i = 0; i < dev->max_ch; i++) {
         free(buff[i]);
     }
-    sdr_dev_close(dev);
 }
 
 //------------------------------------------------------------------------------
 //  Synopsis
 //
 //    pocket_dump [-t tsec] [-r] [-p bus[,port]] [-c conf_file] [-q]
-//                [file [file]]
+//                [file [file ...]]
 //
 //  Description
 //
-//    Capture and dump digital IF data of a SDR device to output files. To stop
-//    capturing, press Ctr-C.
+//    Capture and dump digital IF (DIF) data of a SDR device to output files.
+//    To stop capturing, press Ctr-C.
 //
 //  Options
-//
 //    -t tsec
 //        Data capturing time in seconds.
 //
@@ -157,22 +167,24 @@ static void dump_data(int bus, int port, double tsec, int raw, int quiet,
 //    -q 
 //        Suppress showing data dump status.
 //
-//    [file [file]]
-//        Output digital IF data file paths. The first path is for CH1 and
-//        the second one is for CH2. The second one can be omitted. With
-//        option -r, only the first path is used. If the file path is "",
-//        data are not output to anywhere. If the file path is "-", data are
-//        output to stdout. If all of the file paths omitted, the following
-//        default file paths are used.
+//    [file [file ...]]
+//        Output digital IF data file paths. The first path is for CH1,
+//        the second one is for CH2 and so on. The second one or the later
+//        can be omitted. With option -r, only the first path is used. If
+//        the file path is "", data are not output to anywhere. If the file
+//        path is "-", data are output to stdout. If all of the file paths
+//        omitted, the following default file paths are used.
 //        
 //        CH1: ch1_YYYYMMDD_hhmmss.bin
 //        CH2: ch2_YYYYMMDD_hhmmss.bin
+//        ...
 //        (YYYYMMDD: dump start date in UTC, hhmmss: dump start time in UTC)
 //
 int main(int argc, char **argv)
 {
     FILE *fp[SDR_MAX_CH] = {0};
-    char *files[SDR_MAX_CH], path[2][32];
+    sdr_dev_t *dev;
+    char *files[SDR_MAX_CH] = {0}, path[SDR_MAX_CH][64];
     const char *conf_file = "";
     time_t dump_time;
     double tsec = 0.0;
@@ -201,36 +213,41 @@ int main(int argc, char **argv)
             files[n++] = argv[i];
         }
     }
+    if (*conf_file && !sdr_write_settings(conf_file, bus, port, 0)) {
+        return -1;
+    }
+    if (!(dev = sdr_dev_open(bus, port))) {
+        return -1;
+    }
     if (n == 0) { // set default file paths
         dump_time = time(NULL);
-        strftime(path[0], 32, "ch1_%Y%m%d_%H%M%S.bin", gmtime(&dump_time));
-        strftime(path[1], 32, "ch2_%Y%m%d_%H%M%S.bin", gmtime(&dump_time));
-        files[n++] = path[0];
-        files[n++] = path[1];
+        for (i = 0; i < dev->max_ch; i++) {
+            char *p = path[i];
+            p += sprintf(p, "ch%d_", i + 1);
+            p += strftime(p, 32, "%Y%m%d_%H%M%S.bin", gmtime(&dump_time));
+            files[i] = path[i];
+        }
     }
-    for (i = 0; i < n; i++) {
-        if (raw && i > 0) continue;
+    for (i = 0; i < dev->max_ch; i++) {
+        if (!files[i] || (raw && i > 0)) continue;
         if (!strcmp(files[i], "-")) {
             fp[i] = stdout;
         }
         else if (*files[i] && !(fp[i] = fopen(files[i], "wb"))) {
             fprintf(stderr, "file open error %s\n", files[i]);
+            sdr_dev_close(dev);
             return -1;
         }
     }
     signal(SIGTERM, sig_func);
     signal(SIGINT, sig_func);
     
-    if (*conf_file && !sdr_write_settings(conf_file, bus, port, 0)) {
-        for (i = 0; i < n; i++) {
-            if (fp[i]) fclose(fp[i]);
-        }
-        return -1;
-    }
-    dump_data(bus, port, tsec, raw, quiet, fp);
+    dump_data(dev, tsec, raw, quiet, fp);
     
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < dev->max_ch; i++) {
         if (fp[i]) fclose(fp[i]);
     }
+    sdr_dev_close(dev);
+    
     return 0;
 }
