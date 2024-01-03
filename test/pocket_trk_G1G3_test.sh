bd0ccd795689a1ac50fd09b1a68a62d0f523f80e
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-01-03 23:37:13 +0900

    Add G3OCP.

--- test/pocket_trk_G1G3_test.sh
+++ test/pocket_trk_G1G3_test.sh
@@ -3,7 +3,7 @@
 #  pocket_trk real-time test
 #
 G1="-sig G1CA -prn -7-6"
-G3="-sig G3OCD -prn 1-24"
+G3="-sig G3OCD -prn 1-24 -sig G3OCP -prn 1-24"
 
 ../bin/pocket_dump.exe -r -q -c ../conf/pocket_G1G3_24MHz.conf - |
 ../bin/pocket_trk.exe -r -f 24 $G1 $G3 \
