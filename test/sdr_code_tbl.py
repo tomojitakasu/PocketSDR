b9f783e0f82d4158adc69f319f1fa6b310649414
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-01-07 15:48:35 +0900

    Support I1SD, I1SP, I1SPO.

--- test/sdr_code_tbl.py
+++ test/sdr_code_tbl.py
@@ -61,3 +61,75 @@ for i in range(len(sdr_code.ISS_G2_init)):
         print('')
 print('\n};')
 
+print('static const uint64_t I1SD_R0_init[] = {')
+for i in range(len(sdr_code.I1SD_R0_init)):
+    if i % 4 == 0:
+        print('    ', end='')
+    print('0x%014X, ' % (sdr_code.I1SD_R0_init[i]), end='')
+    if i % 4 == 3:
+        print('')
+print('\n};')
+
+print('static const uint64_t I1SD_R1_init[] = {')
+for i in range(len(sdr_code.I1SD_R1_init)):
+    if i % 4 == 0:
+        print('    ', end='')
+    print('0x%014X, ' % (sdr_code.I1SD_R1_init[i]), end='')
+    if i % 4 == 3:
+        print('')
+print('\n};')
+
+print('static const uint16_t I1SD_C_init[] = {')
+for i in range(len(sdr_code.I1SD_C_init)):
+    if i % 8 == 0:
+        print('    ', end='')
+    print('0x%04X, ' % (sdr_code.I1SD_C_init[i]), end='')
+    if i % 8 == 7:
+        print('')
+print('\n};')
+
+print('static const uint64_t I1SP_R0_init[] = {')
+for i in range(len(sdr_code.I1SP_R0_init)):
+    if i % 4 == 0:
+        print('    ', end='')
+    print('0x%014X, ' % (sdr_code.I1SP_R0_init[i]), end='')
+    if i % 4 == 3:
+        print('')
+print('\n};')
+
+print('static const uint64_t I1SP_R1_init[] = {')
+for i in range(len(sdr_code.I1SP_R1_init)):
+    if i % 4 == 0:
+        print('    ', end='')
+    print('0x%014X, ' % (sdr_code.I1SP_R1_init[i]), end='')
+    if i % 4 == 3:
+        print('')
+print('\n};')
+
+print('static const uint16_t I1SP_C_init[] = {')
+for i in range(len(sdr_code.I1SP_C_init)):
+    if i % 8 == 0:
+        print('    ', end='')
+    print('0x%04X, ' % (sdr_code.I1SP_C_init[i]), end='')
+    if i % 8 == 7:
+        print('')
+print('\n};')
+
+print('static const uint16_t I1SPO_R0_init[] = {')
+for i in range(len(sdr_code.I1SPO_R0_init)):
+    if i % 8 == 0:
+        print('    ', end='')
+    print('0x%04X, ' % (sdr_code.I1SPO_R0_init[i]), end='')
+    if i % 8 == 7:
+        print('')
+print('\n};')
+
+print('static const uint16_t I1SPO_R1_init[] = {')
+for i in range(len(sdr_code.I1SPO_R1_init)):
+    if i % 8 == 0:
+        print('    ', end='')
+    print('0x%04X, ' % (sdr_code.I1SPO_R1_init[i]), end='')
+    if i % 8 == 7:
+        print('')
+print('\n};')
+
