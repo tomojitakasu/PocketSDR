5ee2bc140cb3846ad21d0891aaefc460e86606d4
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-01-12 18:44:48 +0900

    Sampling 32MHz.

--- test/pocket_dump_test.bat
+++ test/pocket_dump_test.bat
@@ -1,7 +1,7 @@
 @echo off
 
 for /l %%i in (0, 1, 10000) do (
-    ..\bin\pocket_dump -t 30 ch1.bin ch2.bin -c ../conf/pocket_L1L5_24MHz.conf
-    python ..\python/pocket_trk.py ch1.bin -f 24 -fi 6 -prn 194
+    ..\bin\pocket_dump -t 30 ch1.bin ch2.bin -c ../conf/pocket_L1L5_32MHz.conf
+    python ..\python/pocket_trk.py ch1.bin -f 32 -fi 6 -prn 194
 )
 
