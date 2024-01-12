43e0686e15f47e6c7083730983ba41644a5b73e1
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-01-12 10:07:50 +0900

    Updated.

--- test/pocket_trk_B1CB2A_test.sh
+++ test/pocket_trk_B1CB2A_test.sh
@@ -2,10 +2,10 @@
 #
 #  pocket_trk real-time test
 #
-B1C="-sig B1CD -prn 19-46 -sig B1CP -prn 19-46"
-B2A="-sig B2AD -prn 19-46 -sig B2AP -prn 19-46"
+B1C="-sig B1CD -prn 19-50 -sig B1CP -prn 19-50"
+B2A="-sig B2AD -prn 19-50 -sig B2AP -prn 19-50"
 
 ../bin/pocket_dump -r -q -c ../conf/pocket_L1L5_24MHz.conf - |
-../bin/pocket_trk -r -f 24 -fi 6,0 $B1C $B2A \
+../bin/pocket_trk -r -f 24 $B1C $B2A \
 -log pocket_trk_B1CB2A_test.log
 
