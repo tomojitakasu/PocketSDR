fb90afaec7820f587e61dc27ee4328b416dad7ff
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-01-16 17:02:58 +0900

    Update max PRN numbers.

--- test/pocket_trk_L1L6_test.sh
+++ test/pocket_trk_L1L6_test.sh
@@ -3,7 +3,7 @@
 #  pocket_trk real-time test
 #
 L1="-sig L1CA -prn 1-32,193-199,120-158"
-L6="-sig L6D -prn 193-199 -sig L6E -prn 203-209"
+L6="-sig L6D -prn 193-202 -sig L6E -prn 203-212"
 
 ../bin/pocket_dump -r -q -c ../conf/pocket_L1L6_12MHz.conf - |
 ../bin/pocket_trk -r -f 12 -fi 3,0 $L1 $L6 \
