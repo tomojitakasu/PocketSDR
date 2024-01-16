b138afaa4b695970bf292d1462c90d57120ff2e9
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-01-16 17:01:59 +0900

    Add L1CA to test signals.

--- test/pocket_trk_L1L2_test.sh
+++ test/pocket_trk_L1L2_test.sh
@@ -2,10 +2,11 @@
 #
 #  pocket_trk real-time test
 #
+L1="-sig L1CA -prn 1-32,193-199"
 L1C="-sig L1CD -prn 1-32,193-199 -sig L1CP -prn 1-32,193-199"
 L2C="-sig L2CM -prn 1-32,193-199"
 
 ../bin/pocket_dump -r -q -c ../conf/pocket_L1L2_6MHz.conf - |
-../bin/pocket_trk -r -f 6 $L1C $L2C \
+../bin/pocket_trk -r -f 6 $L1 $L1C $L2C \
 -log pocket_trk_L1L2_test.log
 
