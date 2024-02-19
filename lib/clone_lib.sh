b1f13134cd2080b8bc84656a5e8049ba896454b3
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-02-20 08:47:01 +0900

    Added.

---
+++ lib/clone_lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#  clone source trees of external libraries
+#
+git clone https://github.com/quiet/libfec
+git clone https://github.com/radfordneal/LDPC-codes
+git clone https://github.com/tomojitakasu/RTKLIB -b rtklib_2.4.3
