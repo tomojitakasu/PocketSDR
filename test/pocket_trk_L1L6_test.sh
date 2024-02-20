cbccc02c25ab921bd12e77696810d22f9b88e590
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-02-20 15:14:32 +0900

    Follow option change of pocket_trk.

--- test/pocket_trk_L1L6_test.sh
+++ test/pocket_trk_L1L6_test.sh
@@ -6,6 +6,6 @@ L1="-sig L1CA -prn 1-32,193-199,120-158"
 L6="-sig L6D -prn 193-202 -sig L6E -prn 203-212"
 
 ../bin/pocket_dump -r -q -c ../conf/pocket_L1L6_12MHz.conf - |
-../bin/pocket_trk -r -f 12 -fi 3,0 $L1 $L6 \
+../bin/pocket_trk -r -IIQ -f 12 -fi 3 $L1 -fi 0 $L6 \
 -log pocket_trk_L1L6_test.log
 
