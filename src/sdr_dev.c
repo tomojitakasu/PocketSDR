ee7a993c665f5da472e2a8152f2f93e01000e32f
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-04-04 19:24:47 +0900

    Refactored.

--- src/sdr_dev.c
+++ src/sdr_dev.c
@@ -12,8 +12,10 @@
 //  2022-07-08  1.2  fix a bug
 //  2022-08-08  1.3  support Spider SDR
 //  2023-12-25  1.3  modify taskname for AvSetMmThreadCharacteristicsA()
+//  2024-04-04  1.4  refactored
 //
 #include "pocket_dev.h"
+#include "pocket_sdr.h"
 #ifdef WIN32
 #include <avrt.h>
 #endif
@@ -21,10 +23,6 @@
 // constants and macros --------------------------------------------------------
 #define TO_TRANSFER     3000    // USB transfer timeout (ms)
 #define TO_TR_LIBUSB    10000   // USB transfer timeout for LIBUSB (ms)
-#ifndef WIN32
-#define MEM_SIZE_USB    256     // USB kernel memory size (MB)
-#define MEM_PAR_USB     "/sys/module/usbcore/parameters/usbfs_memory_mb"
-#endif
 
 // quantization lookup table ---------------------------------------------------
 static int8_t LUT[2][2][256];
@@ -73,6 +71,21 @@ static int read_sample_type(sdr_dev_t *dev)
     return 1;
 }
 
+// read buffer -----------------------------------------------------------------
+static uint8_t *read_buff(sdr_dev_t *dev)
+{
+    int64_t rp = dev->rp, wp = dev->wp;
+    
+    if (rp >= wp) {
+        return NULL;
+    }
+    if (wp > rp + SDR_SIZE_BUFF * SDR_MAX_BUFF) {
+        fprintf(stderr, "bulk transfer buffer overflow\n");
+    }
+    dev->rp = rp + SDR_SIZE_BUFF;
+    return dev->buff + rp % (SDR_SIZE_BUFF * SDR_MAX_BUFF);
+}
+
 #ifdef WIN32
 
 // get bulk transfer endpoint --------------------------------------------------
@@ -86,21 +99,6 @@ static sdr_ep_t *get_bulk_ep(sdr_usb_t *usb, int ep)
     }
     fprintf(stderr, "No bulk end point ep=%02X\n", ep);
     return NULL;
-}
-
-// read buffer -----------------------------------------------------------------
-static uint8_t *read_buff(sdr_dev_t *dev)
-{
-    int rp = dev->rp;
-    
-    if (rp == dev->wp) {
-        return NULL;
-    }
-    if (rp == (dev->wp + 1) % SDR_MAX_BUFF) {
-        fprintf(stderr, "bulk transfer buffer overflow\n");
-    }
-    dev->rp = (rp + 1) % SDR_MAX_BUFF;
-    return dev->buff[rp];
 }
 
 // rise process/thread priority ------------------------------------------------
@@ -125,12 +123,12 @@ static void rise_pri(void)
     }
 }
 
-// event handler thread --------------------------------------------------------
+// USB event handler thread ----------------------------------------------------
 static DWORD WINAPI event_handler(void *arg)
 {
     sdr_dev_t *dev = (sdr_dev_t *)arg;
     uint8_t *ctx[SDR_MAX_BUFF] = {0};
-    OVERLAPPED ov[SDR_MAX_BUFF] = {0};
+    OVERLAPPED ov[SDR_MAX_BUFF] = {{0,0,0,0,0}};
     long len = SDR_SIZE_BUFF;
     
     // rise process/thread priority
@@ -138,23 +136,23 @@ static DWORD WINAPI event_handler(void *arg)
     
     for (int i = 0; i < SDR_MAX_BUFF; i++) {
         ov[i].hEvent = CreateEvent(NULL, false, false, NULL);
-        ctx[i] = dev->ep->BeginDataXfer(dev->buff[i], len, &ov[i]); 
+        ctx[i] = dev->ep->BeginDataXfer(dev->buff + len * i, len, &ov[i]); 
     }
     for (int i = 0; dev->state; ) {
         if (!dev->ep->WaitForXfer(&ov[i], TO_TRANSFER)) {
             fprintf(stderr, "bulk transfer timeout\n");
             continue;
         }
-        if (!dev->ep->FinishDataXfer(dev->buff[i], len, &ov[i], ctx[i])) {
+        if (!dev->ep->FinishDataXfer(dev->buff + len * i, len, &ov[i], ctx[i])) {
             fprintf(stderr, "bulk transfer error\n");
             break;
         }
-        ctx[i] = dev->ep->BeginDataXfer(dev->buff[i], len, &ov[i]);
-        dev->wp = i;
+        ctx[i] = dev->ep->BeginDataXfer(dev->buff + len * i, len, &ov[i]);
+        dev->wp += len;
         i = (i + 1) % SDR_MAX_BUFF;
     }
     for (int i = 0; i < SDR_MAX_BUFF; i++) {
-        dev->ep->FinishDataXfer(dev->buff[i], len, &ov[i], ctx[i]);
+        dev->ep->FinishDataXfer(dev->buff + len * i, len, &ov[i], ctx[i]);
         CloseHandle(ov[i].hEvent);
     }
     return 0;
@@ -162,53 +160,41 @@ static DWORD WINAPI event_handler(void *arg)
 
 #else // WIN32
 
-// write ring-buffer -----------------------------------------------------------
-static int write_buff(sdr_dev_t *dev, uint8_t *data)
-{
-    int wp = (dev->wp + 1) % SDR_MAX_BUFF;
-    if (wp == dev->rp) {
-        return 0;
-    }
-    dev->buff[wp] = data;
-    dev->wp = wp;
-    return 1;
-}
-
-// read ring-buffer ------------------------------------------------------------
-static uint8_t *read_buff(sdr_dev_t *dev)
-{
-    uint8_t *data;
-    
-    if (dev->rp == dev->wp) {
-        return NULL;
-    }
-    data = dev->buff[dev->rp];
-    dev->rp = (dev->rp + 1) % SDR_MAX_BUFF;
-    return data;
-}
-
 // USB bulk transfer callback --------------------------------------------------
 static void transfer_cb(struct libusb_transfer *transfer)
 {
     sdr_dev_t *dev = (sdr_dev_t *)transfer->user_data;
     
     if (transfer->status != LIBUSB_TRANSFER_COMPLETED) {
-        fprintf(stderr, "USB bulk transfer error (%d)\n", transfer->status);
+        fprintf(stderr, "libusb bulk transfer error (%d)\n", transfer->status);
     }
-    else if (!write_buff(dev, transfer->buffer)) {
-        fprintf(stderr, "USB bulk transfer buffer overflow\n");
-    }
+    dev->wp += SDR_SIZE_BUFF;
     libusb_submit_transfer(transfer);
 }
 
 // USB event handler thread ----------------------------------------------------
-static void *event_handler_thread(void *arg)
+static void *event_handler(void *arg)
 {
     sdr_dev_t *dev = (sdr_dev_t *)arg;
+    struct sched_param param = {99};
     struct timeval to = {0, 100000};
     
+    // set thread scheduling real-time
+    if (pthread_setschedparam(dev->thread, SCHED_RR, &param)) {
+        fprintf(stderr, "set thread scheduling error\n");
+    }
+    for (int i = 0; i < SDR_MAX_BUFF; i++) {
+        int ret = libusb_submit_transfer(dev->transfer[i]);
+        if (ret) {
+            fprintf(stderr, "libusb_submit_transfer(%d) error (%d)\n", i, ret);
+            return NULL;
+        }
+    }
     while (dev->state) {
         if (libusb_handle_events_timeout(NULL, &to)) continue;
+    }
+    for (int i = 0; i < SDR_MAX_BUFF; i++) {
+        libusb_cancel_transfer(dev->transfer[i]);
     }
     return NULL;
 }
@@ -245,29 +231,18 @@ sdr_dev_t *sdr_dev_open(int bus, int port)
         fprintf(stderr, "Read sampling type error\n");
         return NULL;
     }
-    for (int i = 0; i < SDR_MAX_BUFF; i++) {
-        dev->buff[i] = new uint8_t[SDR_SIZE_BUFF];
-    }
+    dev->buff = new uint8_t[SDR_SIZE_BUFF * SDR_MAX_BUFF];
     dev->ep->SetXferSize(SDR_SIZE_BUFF);
     gen_LUT();
     
     dev->state = 1;
     dev->rp = dev->wp = 0;
     dev->thread = CreateThread(NULL, 0, event_handler, dev, 0, NULL);
-    
     return dev;
     
 #else // WIN32
     sdr_dev_t *dev;
-    struct sched_param param = {99};
-    char buff[1024];
-    int ret;
     
-    // increase kernel memory size for USB FS
-    sprintf(buff, "echo %d > %s\n", MEM_SIZE_USB, MEM_PAR_USB);
-    if (system(buff)) {
-        fprintf(stderr, "Kernel memory size for USB setting error\n");
-    }
     if (!(dev = (sdr_dev_t *)malloc(sizeof(sdr_dev_t)))) {
         return NULL;
     }
@@ -281,35 +256,24 @@ sdr_dev_t *sdr_dev_open(int bus, int port)
         fprintf(stderr, "Read sampling type error\n");
         return NULL;
     }
+    dev->buff = (uint8_t *)malloc(SDR_SIZE_BUFF * SDR_MAX_BUFF);
     for (int i = 0; i < SDR_MAX_BUFF; i++) {
-#if 1
-        dev->data[i] = libusb_dev_mem_alloc(dev->usb->h, SDR_SIZE_BUFF);
-#else
-        dev->data[i] = (uint8_t *)malloc(SDR_SIZE_BUFF);
-#endif
-        if (!(dev->transfer[i] = libusb_alloc_transfer(0))) {
+        dev->transfer[i] = libusb_alloc_transfer(0);
+        if (!dev->transfer[i]) {
+            fprintf(stderr, "libusb_alloc_transfer(%d) error\n", i);
             sdr_usb_close(dev->usb);
             free(dev);
             return NULL;
         }
         libusb_fill_bulk_transfer(dev->transfer[i], dev->usb->h, SDR_DEV_EP,
-            dev->data[i], SDR_SIZE_BUFF, transfer_cb, dev, TO_TR_LIBUSB);
+            dev->buff + SDR_SIZE_BUFF * i, SDR_SIZE_BUFF, transfer_cb, dev,
+            TO_TR_LIBUSB);
     }
     gen_LUT();
     
     dev->state = 1;
     dev->rp = dev->wp = 0;
-    pthread_create(&dev->thread, NULL, event_handler_thread, dev);
-    
-    // set thread scheduling real-time
-    if (pthread_setschedparam(dev->thread, SCHED_RR, &param)) {
-        fprintf(stderr, "set thread scheduling error\n");
-    }
-    for (int i = 0; i < SDR_MAX_BUFF; i++) {
-        if ((ret = libusb_submit_transfer(dev->transfer[i]))) {
-            fprintf(stderr, "libusb_submit_transfer(%d) error (%d)\n", i, ret);
-        }
-    }
+    pthread_create(&dev->thread, NULL, event_handler, dev);
     return dev;
 
 #endif // WIN32
@@ -330,29 +294,17 @@ void sdr_dev_close(sdr_dev_t *dev)
     dev->state = 0;
     WaitForSingleObject(dev->thread, 10000);
     CloseHandle(dev->thread);
-    
-    for (int i = 0; i < SDR_MAX_BUFF; i++) {
-        delete [] dev->buff[i];
-    }
+    delete [] dev->buff;
     sdr_usb_close(dev->usb);
     delete dev;
 #else
     dev->state = 0;
     pthread_join(dev->thread, NULL);
-    
-    for (int i = 0; i < SDR_MAX_BUFF; i++) {
-        libusb_cancel_transfer(dev->transfer[i]);
-    }
     sdr_usb_close(dev->usb);
-    
     for (int i = 0; i < SDR_MAX_BUFF; i++) {
         libusb_free_transfer(dev->transfer[i]);
-#if 1
-        libusb_dev_mem_free(dev->usb->h, dev->data[i], SDR_SIZE_BUFF);
-#else
-        free(dev->data[i]);
-#endif
     }
+    free(dev->buff);
     free(dev);
 #endif // WIN32
 }
@@ -400,7 +352,26 @@ static int copy_data(const uint8_t *data, int ch, int IQ, int8_t *buff)
     return size;
 }
 
-// get digital IF data ---------------------------------------------------------
+// read raw IF data -----------------------------------------------------------
+int sdr_dev_read(sdr_dev_t *dev, uint8_t *buff, int n)
+{
+    int size = SDR_SIZE_BUFF * SDR_MAX_BUFF, rp = (int)(dev->rp % size);
+    
+    if (dev->wp < dev->rp + n) {
+        return 0;
+    }
+    if (rp + n <= size) {
+        memcpy(buff, dev->buff + rp, n);
+    }
+    else {
+        memcpy(buff, dev->buff + rp, size - rp);
+        memcpy(buff + size - rp, dev->buff, n - size + rp);
+    }
+    dev->rp += n;
+    return n;
+}
+
+// read decoded IF data --------------------------------------------------------
 int sdr_dev_data(sdr_dev_t *dev, int8_t **buff, int *n)
 {
     uint8_t *data;
@@ -409,7 +380,7 @@ int sdr_dev_data(sdr_dev_t *dev, int8_t **buff, int *n)
     for (int i = 0; i < dev->max_ch; i++) {
         n[i] = 0;
     }
-    while ((data = read_buff(dev))) {
+    while ((data = read_buff(dev)) && size < SDR_SIZE_BUFF * SDR_MAX_BUFF) {
         for (int i = 0; i < dev->max_ch; i++) {
             n[i] += copy_data(data, i, dev->IQ[i], buff[i] + n[i]);
         }
