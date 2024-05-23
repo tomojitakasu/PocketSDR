13bd844789889ee3fefab8a1d859a5f0a486443b
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-05-23 19:16:59 +0900

    Support Pocket SDR FE 4CH.

--- app/pocket_dump/pocket_dump.c
+++ app/pocket_dump/pocket_dump.c
@@ -14,6 +14,7 @@
 //                   DATA_CYC: 50 -> 10 (ms)
 //  2023-12-25  1.4  insert wait after writing receiver settings
 //  2023-12-28  1.5  set binary mode to stdout for Windows
+//  2023-04-28  1.6  support Pocket SDR FE 4CH
 //
 #include <signal.h>
 #include <time.h>
@@ -28,34 +29,10 @@
 #define DATA_CYC        10      // data capture cycle (ms)
 #define STAT_CYC        50      // status update cycle (ms)
 #define RATE_CYC        1000    // data rate update cycle (ms)
+#define F2BIT(buff,ch,IQ) (((buff)>>(4*(ch)+2*(IQ)))&3)
 
 // interrupt flag --------------------------------------------------------------
 static volatile uint8_t intr = 0;
-
-// get system tick (ms) --------------------------------------------------------
-static uint32_t get_tick(void)
-{
-#ifdef WIN32
-    return (uint32_t)timeGetTime();
-#else
-    struct timeval tv = {0};
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec * 1000u + tv.tv_usec / 1000u;
-#endif
-}
-
-// sleep milli-sec -------------------------------------------------------------
-static void sleep_msec(int msec)
-{
-#ifdef WIN32
-    Sleep(msec < 5 ? 1 : msec);
-#else
-    struct timespec ts = {0};
-    if (msec <= 0) return;
-    ts.tv_nsec = (long)(msec * 1000000);
-    nanosleep(&ts, NULL);
-#endif
-}
 
 // signal handler --------------------------------------------------------------
 static void sig_func(int sig)
@@ -72,46 +49,117 @@ static void print_usage(void)
     exit(0);
 }
 
-// dump digital IF data --------------------------------------------------------
-static void dump_data(sdr_dev_t *dev, double tsec, int raw, int quiet,
-    FILE **fp)
+// generate lookup table -------------------------------------------------------
+static void gen_LUT(int8_t LUT[][256])
+{
+    static const int8_t val[] = {1, 3, -1, -3}; // sign + magnitude
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 256; j++) {
+            LUT[i][j] = val[(j >> (i * 2)) & 3];
+        }
+    }
+}
+
+// write IF data to file -------------------------------------------------------
+static int write_file(int fmt, const uint8_t *buff, int size, int ch, int IQ,
+    FILE *fp)
+{
+    static int8_t LUT[4][256] = {{0}};
+    
+    if (!fp) return 0;
+    
+    if (fmt == 0) { // output raw
+        return (int)fwrite(buff, 1, size, fp);
+    }
+    if (!LUT[0][0]) {
+        gen_LUT(LUT);
+    }
+    int8_t data[size * IQ];
+    
+    if (fmt == SDR_FMT_RAW8) { // packed 8(4x2) bits raw
+        int pos = ch * 2;
+        for (int i = 0; i < size; i++) {
+            if (IQ == 1) {
+                data[i] = LUT[pos][buff[i]];
+            }
+            else {
+                data[i*2  ] = LUT[pos  ][buff[i]];
+                data[i*2+1] = LUT[pos+1][buff[i]];
+            }
+        }
+    }
+    else if (fmt == SDR_FMT_RAW16) { // packed 16(4x4) bits raw
+        int pos = ch % 2 * 2;
+        for (int i = 0, j = ch / 2; i < size; i++, j += 2) {
+            if (IQ == 1) {
+                data[i] = LUT[pos][buff[j]];
+            }
+            else {
+                data[i*2  ] = LUT[pos  ][buff[j]];
+                data[i*2+1] = LUT[pos+1][buff[j]];
+            }
+        }
+    }
+    else { // packed 16(2x8) bits raw
+        int pos = ch % 4;
+        for (int i = 0, j = ch / 4; i < size; i++, j += 2) {
+            data[i] = LUT[pos][buff[j]];
+        }
+    }
+    return (int)fwrite(data, 1, size * IQ, fp);
+}
+
+// print header ----------------------------------------------------------------
+static void print_head(int nch, FILE **fp)
+{
+    fprintf(stderr, "%9s ", "TIME(s)");
+    for (int i = 0; i < nch; i++) {
+        if (fp[i]) fprintf(stderr, " %3s   CH%d(Bytes)", "T", i + 1);
+    }
+    fprintf(stderr, " %12s\n", "RATE(Ks/s)");
+}
+
+// print status ----------------------------------------------------------------
+static void print_stat(int nch, const int *IQ, FILE **fp, double time,
+    const double *byte, double rate)
 {
     static const char *str_IQ[] = {"-", "I", "IQ", "I"};
+    
+    fprintf(stderr, "%9.1f ", time);
+    for (int i = 0; i < nch; i++) {
+        if (!fp[i]) continue;
+        fprintf(stderr, " %3s %12.0f", str_IQ[IQ[i]], byte[i]);
+    }
+    fprintf(stderr, " %12.1f\r", rate * 1e-3);
+    fflush(stderr);
+}
+
+// dump digital IF data --------------------------------------------------------
+static void dump_data(sdr_dev_t *dev, double tsec, int quiet, int fmt,
+    double fs, int nch, const double *fo, const int *IQ, FILE **fp)
+{
     double time = 0.0, time_p = 0.0, sample = 0.0, sample_p = 0.0;
     double rate = 0.0, byte[SDR_MAX_CH] = {0};
-    int8_t *buff[SDR_MAX_CH];
-    int size, n[SDR_MAX_CH];
+    int ns = (fmt == 0 || fmt == SDR_FMT_RAW8) ? 1 : 2;
+    uint8_t buff[SDR_SIZE_BUFF * ns];
     
-    if (raw) {
-        for (int i = 0; i < dev->max_ch; i++) {
-            dev->IQ[i] = 0;
-        }
-    }
-    for (int i = 0; i < dev->max_ch; i++) {
-        size = SDR_SIZE_BUFF * SDR_MAX_BUFF * (dev->IQ[i] == 2 ? 2 : 1);
-        buff[i] = (int8_t *)malloc(size);
-    }
     if (!quiet) {
-        fprintf(stderr, "%9s ", "TIME(s)");
-        for (int i = 0; i < dev->max_ch; i++) {
-            if (!fp[i]) continue;
-            fprintf(stderr, " %3s   CH%d(Bytes)", "T", i + 1);
-        }
-        fprintf(stderr, " %12s\n", "RATE(Ks/s)");
+        print_head(nch, fp);
     }
-    uint32_t tick = get_tick();
+    uint32_t tick = sdr_get_tick();
+    
+    if (!sdr_dev_start(dev)) return;
     
     for (int i = 0; !intr && (tsec <= 0.0 || time < tsec); i++) {
         if (!quiet) {
-            time = (get_tick() - tick) * 1e-3;
+            time = (sdr_get_tick() - tick) * 1e-3;
         }
-        if ((size = sdr_dev_data(dev, buff, n)) > 0) {
-            for (int j = 0; j < dev->max_ch; j++) {
-                if (!fp[j]) continue;
-                fwrite(buff[j], n[j], 1, fp[j]);
-                byte[j] += n[j];
+        while (sdr_dev_read(dev, buff, SDR_SIZE_BUFF * ns) && !intr) {
+            for (int j = 0; j < nch; j++) {
+                byte[j] += write_file(fmt, buff, SDR_SIZE_BUFF, j, IQ[j], fp[j]);
             }
-            sample += size;
+            sample += SDR_SIZE_BUFF;
         }
         if (!quiet && time - time_p > RATE_CYC * 1e-3) {
             rate = (sample - sample_p) / (time - time_p);
@@ -119,27 +167,16 @@ static void dump_data(sdr_dev_t *dev, double tsec, int raw, int quiet,
             sample_p = sample;
         }
         if (!quiet && i % (STAT_CYC / DATA_CYC) == 0) {
-            fprintf(stderr, "%9.1f ", time);
-            for (int j = 0; j < dev->max_ch; j++) {
-                if (!fp[j]) continue;
-                fprintf(stderr, " %3s %12.0f", str_IQ[dev->IQ[j]], byte[j]);
-            }
-            fprintf(stderr, " %12.1f\r", rate * 1e-3);
-            fflush(stderr);
+            print_stat(nch, IQ, fp, time, byte, rate);
         }
-        sleep_msec(DATA_CYC);
+        sdr_sleep_msec(DATA_CYC);
     }
+    (void)sdr_dev_stop(dev);
+    
     if (!quiet) {
         rate = time > 0.0 ? sample / time : 0.0;
-        fprintf(stderr, "%9.1f ", time);
-        for (int j = 0; j < dev->max_ch; j++) {
-            if (!fp[j]) continue;
-            fprintf(stderr, " %3s %12.0f", str_IQ[dev->IQ[j]], byte[j]);
-        }
-        fprintf(stderr, " %12.1f\n", rate * 1e-3);
-    }
-    for (int i = 0; i < dev->max_ch; i++) {
-        free(buff[i]);
+        print_stat(nch, IQ, fp, time, byte, rate);
+        fprintf(stderr, "\n");
     }
 }
 
@@ -192,15 +229,16 @@ int main(int argc, char **argv)
     char *files[SDR_MAX_CH] = {0}, path[SDR_MAX_CH][64];
     const char *conf_file = "";
     time_t dump_time;
-    double tsec = 0.0;
+    double tsec = 0.0, fs = 0.0, fo[SDR_MAX_CH] = {0};
     int i, n = 0, bus = -1, port = -1, raw = 0, quiet = 0;
+    int fmt = 0, nch = 1, IQ[SDR_MAX_CH] = {0};
     
     for (i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "-t") && i + 1 < argc) {
             tsec = atof(argv[++i]);
         }
         else if (!strcmp(argv[i], "-r")) {
-            raw = 1; /* raw */
+            raw = 1; // raw output
         }
         else if (!strcmp(argv[i], "-p") && i + 1 < argc) {
             sscanf(argv[++i], "%d,%d", &bus, &port);
@@ -222,22 +260,29 @@ int main(int argc, char **argv)
         if (!sdr_write_settings(conf_file, bus, port, 0)) {
             return -1;
         }
-        sleep_msec(100);
+        sdr_sleep_msec(100);
     }
     if (!(dev = sdr_dev_open(bus, port))) {
         return -1;
     }
+    if (!raw) {
+        if (!(fmt = sdr_dev_info(dev, &fs, fo, IQ))) {
+            sdr_dev_close(dev);
+            return -1;
+        }
+        nch = fmt == SDR_FMT_RAW8 ? 2 : (fmt == SDR_FMT_RAW16 ? 4 : 8);
+    }
     if (n == 0) { // set default file paths
         dump_time = time(NULL);
-        for (i = 0; i < dev->max_ch; i++) {
+        for (i = 0; i < nch; i++) {
             char *p = path[i];
             p += sprintf(p, "ch%d_", i + 1);
             p += strftime(p, 32, "%Y%m%d_%H%M%S.bin", gmtime(&dump_time));
             files[i] = path[i];
         }
     }
-    for (i = 0; i < dev->max_ch; i++) {
-        if (!files[i] || (raw && i > 0)) continue;
+    for (i = 0; i < nch; i++) {
+        if (!files[i]) continue;
         if (!strcmp(files[i], "-")) {
 #ifdef WIN32 // set binary mode for Windows
             _setmode(_fileno(stdout), _O_BINARY);
@@ -252,11 +297,10 @@ int main(int argc, char **argv)
     }
     signal(SIGTERM, sig_func);
     signal(SIGINT, sig_func);
-    sdr_sleep_msec(500);
     
-    dump_data(dev, tsec, raw, quiet, fp);
+    dump_data(dev, tsec, quiet, fmt, fs, nch, fo, IQ, fp);
     
-    for (i = 0; i < dev->max_ch; i++) {
+    for (i = 0; i < nch; i++) {
         if (fp[i]) fclose(fp[i]);
     }
     sdr_dev_close(dev);
