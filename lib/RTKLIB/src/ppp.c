a3f6d1bd7cc76e0dfc18403d54d3e015f28568be
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-05-23 19:21:50 +0900

    Suppress warning.

--- lib/RTKLIB/src/ppp.c
+++ lib/RTKLIB/src/ppp.c
@@ -407,7 +407,7 @@ static void detslp_ll(rtk_t *rtk, const obsd_t *obs, int n)
     
     trace(3,"detslp_ll: n=%d\n",n);
     
-    for (i=0;i<n&&i<MAXOBS;i++) for (j=0;j<rtk->opt.nf;j++) {
+    for (i=0;i<n&&i<MAXOBS;i++) for (j=0;j<rtk->opt.nf&&j<NFREQ;j++) {
         if (obs[i].L[j]==0.0||!(obs[i].LLI[j]&3)) continue;
         
         trace(3,"detslp_ll: slip detected sat=%2d f=%d\n",obs[i].sat,j+1);
