6e922d28ce1bd4bb3a29849328cc5e959095994a
Tomoji Takasu <ttaka@yk.rim.or.jp>
2024-07-03 05:37:51 +0900

    Added.

---
+++ FE_4CH/FW/v3.0/pocket_fw_v3/Release/subdir.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../cyfxtx.c \
+../pocket_fw_v3.c \
+../pocket_usb_dscr.c 
+
+S_UPPER_SRCS += \
+../cyfx_gcc_startup.S 
+
+OBJS += \
+./cyfx_gcc_startup.o \
+./cyfxtx.o \
+./pocket_fw_v3.o \
+./pocket_usb_dscr.o 
+
+C_DEPS += \
+./cyfxtx.d \
+./pocket_fw_v3.d \
+./pocket_usb_dscr.d 
+
+S_UPPER_DEPS += \
+./cyfx_gcc_startup.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU Assembler'
+	arm-none-eabi-gcc -mcpu=arm926ej-s -marm -mthumb-interwork -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -x assembler-with-cpp -I"C:\Program Files (x86)\Cypress\EZ-USB FX3 SDK\1.3\/fw_lib/1_3_5/inc" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=arm926ej-s -marm -mthumb-interwork -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -D__CYU3P_TX__=1 -DREV_A -I"C:\Program Files (x86)\Cypress\EZ-USB FX3 SDK\1.3\/fw_lib/1_3_5/inc" -std=c99 -v -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
