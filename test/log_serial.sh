6379efe58c7b0f2d457df7a69ae13db220058ff5
tomojitakasu <ttaka@yk.rim.or.jp>
2025-03-07 10:56:48 +0900

    Updated.

--- test/log_serial.sh
+++ test/log_serial.sh
@@ -3,9 +3,9 @@
 # Log of Serial Device
 #
 ISTR=serial://ttyACM1:115200
-OSTR1=../log/mosaic_%Y%m%d%h%M.sbf::S=1
+OSTR1=../../log/mosaic_%Y%m%d%h%M.sbf::S=1
 OSTR2=tcpsvr://:10016
-LOG=../log/log_serial.log
+LOG=../../log/log_serial.log
 
 ../bin/str2str -in $ISTR -out $OSTR1 -out $OSTR2 # 2> $LOG
 
