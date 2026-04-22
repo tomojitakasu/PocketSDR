2bf788019c0fd3cf52471ccdbdc6fe9c3a53917e
ttaka <ttaka@yk.rim.or.jp>
2026-04-22 22:36:19 +0900

    Followed thread API changes.

--- src/sdr_sdev.c
+++ src/sdr_sdev.c
@@ -1,26 +1,34 @@
 //
 //  Pocket SDR C Library - SoapySDR Device Functions.
 //
+//  Options:
+//      -DSOAPYSDR: enable SoapySDR device functions
+//
 //  Author:
 //  T.TAKASU
 //
 //  History:
 //  2025-11-17  0.1  new
 //
+#ifdef SOAPYSDR
 #include <SoapySDR/Device.h>
 #include <SoapySDR/Formats.h>
-#include <signal.h>
+#endif
 #include "pocket_sdr.h"
+#ifdef WIN32
+#include <avrt.h>
+#endif
 
-#define BUFF_SIZE   (1<<14)      // buffer size (bytes)
+#define BUFF_SIZE   (1<<16)      // buffer size (bytes)
 #define BUFF_CNT    256          // buffer count
 #define READ_TO     100000       // read timeout (us)
 
 // list SoapySDR devices -------------------------------------------------------
 int sdr_sdev_list(void)
 {
-    SoapySDRKwargs *devs = NULL;
     size_t len = 0;
+#ifdef SOAPYSDR
+    SoapySDRKwargs *devs = NULL;
     const char *label, *driver, *serial;
     
     devs = SoapySDRDevice_enumerate(NULL, &len);
@@ -34,11 +42,13 @@ int sdr_sdev_list(void)
         label = SoapySDRKwargs_get(devs + i, "label");
         driver = SoapySDRKwargs_get(devs + i, "driver");
         serial = SoapySDRKwargs_get(devs + i, "serial");
-        printf("#%d: %s | driver=%8s | serial=%8s\n", i, label ? label : "(no label)",
-            driver ? driver : "?", serial ? serial : "");
+        printf("#%d: %s | driver=%8s | serial=%8s\n", i,
+            label ? label : "(no label)", driver ? driver : "?",
+            serial ? serial : "");
     }
     SoapySDRKwargsList_clear(devs, len);
-    return len;
+#endif
+    return (int)len;
 }
 
 //------------------------------------------------------------------------------
@@ -48,9 +58,10 @@ int sdr_sdev_list(void)
 //      driver      (I)   SoapySDR driver
 //                          "uhd"     : USRP
 //                          "lime"    : LimeSDR
+//                          "bladrf"  : bladeRF
 //                          "rtlsdr"  : RTL-SDR
-//                          "plutosdr": Pluto-SDR
-//      fmt         (I)   sampling data format (CS8 or CS16)
+//                          "plutosdr": PlutoSDR
+//      fmt         (I)   sampling data format (SDR_FMT_CS8 or _CS16)
 //      rate        (I)   sampling rate (sps)
 //      freq        (I)   carrier center frequency (Hz)
 //      bw          (I)   filter bandwidth (Hz) (<= 0: not set)
@@ -62,18 +73,21 @@ int sdr_sdev_list(void)
 sdr_sdev_t *sdr_sdev_open(const char *driver, int fmt, double rate,
     double freq, double bw, double gain)
 {
+#ifdef SOAPYSDR
     SoapySDRDevice *dev;
     SoapySDRStream *str;
     SoapySDRKwargs args = SoapySDRKwargs_fromString("");
     const char *str_fmt;
     const size_t ch = 0;
     size_t chs[1] = {ch};
+printf("sdr_sdev_open driver=%s fmt=%d rate=%.0f freq=%.0f bw=%.0f gain=%.0f\n", driver, fmt, rate, freq, bw, gain);
     
     if (fmt != SDR_FMT_CS8 && fmt != SDR_FMT_CS16) {
         return NULL;
     }
     SoapySDRKwargs_set(&args, "driver", driver);
     if (!(dev = SoapySDRDevice_make(&args))) {
+        fprintf(stderr, "SoapySDRDevice_make error\n");
         SoapySDRKwargs_clear(&args);
         return NULL;
     }
@@ -84,8 +98,7 @@ sdr_sdev_t *sdr_sdev_open(const char *driver, int fmt, double rate,
     if (gain > 0.0) {
         (void)SoapySDRDevice_setGainMode(dev, SOAPY_SDR_RX, ch, false);
         (void)SoapySDRDevice_setGain(dev, SOAPY_SDR_RX, ch, gain);
-    }
-    else {
+    } else {
         (void)SoapySDRDevice_setGainMode(dev, SOAPY_SDR_RX, ch, true);
     }
     if (bw > 0.0) {
@@ -93,9 +106,9 @@ sdr_sdev_t *sdr_sdev_open(const char *driver, int fmt, double rate,
     }
     (void)SoapySDRDevice_setDCOffsetMode(dev, SOAPY_SDR_RX, ch, true);
     (void)SoapySDRDevice_setIQBalanceMode(dev, SOAPY_SDR_RX, ch, true);
-
+    
     str_fmt = fmt == SDR_FMT_CS8 ? SOAPY_SDR_CS8 : SOAPY_SDR_CS16;
-
+    
     if (!(str = SoapySDRDevice_setupStream(dev, SOAPY_SDR_RX, str_fmt, chs, 1,
         NULL))) {
         fprintf(stderr, "SoapySDRDevice_setupStream error\n");
@@ -109,8 +122,11 @@ sdr_sdev_t *sdr_sdev_open(const char *driver, int fmt, double rate,
     sdev->ssize = fmt == SDR_FMT_CS8 ? 2 : 4;
     sdev->buff = (uint8_t *)sdr_malloc(BUFF_SIZE * BUFF_CNT * sdev->ssize);
     sdev->rp = sdev->wp = 0;
-    pthread_mutex_init(&sdev->mtx, NULL);
+    sdr_mutex_init(&sdev->mtx);
     return sdev;
+#else
+    return NULL;
+#endif
 }
 
 //------------------------------------------------------------------------------
@@ -124,6 +140,7 @@ sdr_sdev_t *sdr_sdev_open(const char *driver, int fmt, double rate,
 //
 void sdr_sdev_close(sdr_sdev_t *sdev)
 {
+#ifdef SOAPYSDR
     if (!sdev) return;
     SoapySDRDevice *dev = (SoapySDRDevice *)sdev->dev;
     SoapySDRStream *str = (SoapySDRStream *)sdev->str;
@@ -131,33 +148,48 @@ void sdr_sdev_close(sdr_sdev_t *sdev)
     SoapySDRDevice_unmake(dev);
     sdr_free(sdev->buff);
     sdr_free(sdev);
+#endif
 }
 
 // Soapy device reader thread --------------------------------------------------
+#ifdef SOAPYSDR
 static void *reader_thread(void *arg)
 {
     sdr_sdev_t *sdev = (sdr_sdev_t *)arg;
     SoapySDRDevice *dev = (SoapySDRDevice *)sdev->dev;
     SoapySDRStream *str = (SoapySDRStream *)sdev->str;
     void *buffs[1];
-    int ret, flags;
+    int ret, flags, ovcnt = 0;
     long long tns;
     
-    for (int i = 0; sdev->state; i = (i + 1) % BUFF_CNT) {
+    sdev->rp = sdev->wp = 0;
+    
+    for (int i = 0; sdev->state; ) {
         buffs[0] = (void *)(sdev->buff + BUFF_SIZE * i);
         ret = SoapySDRDevice_readStream(dev, str, buffs, BUFF_SIZE / sdev->ssize,
             &flags, &tns, READ_TO);
         if (ret == SOAPY_SDR_TIMEOUT) continue;
+        if (ret == SOAPY_SDR_OVERFLOW) {
+            ovcnt++;
+            continue;
+        }
         if (ret < 0) {
-            fprintf(stderr, "device read error (%s)\n", SoapySDR_errToStr(ret));
+            fprintf(stderr, "SoapySDRDevice_readStream error (%s)\n",
+                SoapySDR_errToStr(ret));
             break;
         }
-        pthread_mutex_lock(&sdev->mtx);
-        sdev->wp += BUFF_SIZE;
-        pthread_mutex_unlock(&sdev->mtx);
+        sdr_mutex_lock(&sdev->mtx);
+        sdev->wp += ret * sdev->ssize;
+        sdr_mutex_unlock(&sdev->mtx);
+        i = (i + 1) % BUFF_CNT;
+    }
+    if (ovcnt > 0) {
+        fprintf(stderr, "SoapySDRDevice_readStream overflow cnt=%d\n",
+            ovcnt);
     }
     return NULL;
 }
+#endif
 
 //------------------------------------------------------------------------------
 //  Start SoapySDR device.
@@ -170,13 +202,21 @@ static void *reader_thread(void *arg)
 //
 int sdr_sdev_start(sdr_sdev_t *sdev)
 {
+#ifdef SOAPYSDR
     if (!sdev || sdev->state) return 0;
     SoapySDRDevice *dev = (SoapySDRDevice *)sdev->dev;
     SoapySDRStream *str = (SoapySDRStream *)sdev->str;
+    if (SoapySDRDevice_activateStream(dev, str, 0, 0, 0)) return 0;
     sdev->state = 1;
-    sdev->rp = sdev->wp = 0;
-    pthread_create(&sdev->thread, NULL, reader_thread, sdev);
-    return SoapySDRDevice_activateStream(dev, str, 0, 0, 0) == 0;
+    if (!sdr_thread_create(&sdev->thread, reader_thread, sdev)) {
+        sdev->state = 0;
+        SoapySDRDevice_deactivateStream(dev, str, 0, 0);
+        return 0;
+    }
+    return 1;
+#else
+    return 0;
+#endif
 }
 
 //------------------------------------------------------------------------------
@@ -190,13 +230,17 @@ int sdr_sdev_start(sdr_sdev_t *sdev)
 //
 int sdr_sdev_stop(sdr_sdev_t *sdev)
 {
+#ifdef SOAPYSDR
     if (!sdev || !sdev->state) return 0;
     SoapySDRDevice *dev = (SoapySDRDevice *)sdev->dev;
     SoapySDRStream *str = (SoapySDRStream *)sdev->str;
     SoapySDRDevice_deactivateStream(dev, str, 0, 0);
     sdev->state = 0;
-    pthread_join(sdev->thread, NULL);
+    sdr_thread_join(sdev->thread);
     return 1;
+#else
+    return 0;
+#endif
 }
 
 //------------------------------------------------------------------------------
@@ -217,9 +261,9 @@ int sdr_sdev_read(sdr_sdev_t *sdev, uint8_t *buff, int size)
     
     if (!sdev || !sdev->state) return 0;
     
-    pthread_mutex_lock(&sdev->mtx);
+    sdr_mutex_lock(&sdev->mtx);
     wp = sdev->wp;
-    pthread_mutex_unlock(&sdev->mtx);
+    sdr_mutex_unlock(&sdev->mtx);
     
     bsize = (int)(wp - sdev->rp);
     
@@ -234,8 +278,7 @@ int sdr_sdev_read(sdr_sdev_t *sdev, uint8_t *buff, int size)
     
     if (pos + size <= BUFF_SIZE * BUFF_CNT) {
         memcpy(buff, sdev->buff + pos, size);
-    }
-    else {
+    } else {
         psize = BUFF_SIZE * BUFF_CNT - pos;
         memcpy(buff, sdev->buff + pos, psize);
         memcpy(buff + psize, sdev->buff, size - psize);
