c12070463a584a6d094fa6fac6169329d9d0797c
ttaka <ttaka@yk.rim.or.jp>
2026-04-22 22:47:27 +0900

    Added API sdr_thread_create(), sdr_thread_join(), sdr_mutex_init(), sdr_mutex_lock(), sdr_mutex_unlock().

--- src/sdr_cmn.c
+++ src/sdr_cmn.c
@@ -10,9 +10,14 @@
 //  2022-05-17  1.1  add API sdr_cpx_malloc(), sdr_cpx_free()
 //  2022-07-08  1.2  add API sdr_get_time()
 //                   move API sdr_cpx_malloc(), sdr_cpx_free() to sdr_func.c
+//  2026-02-12  1.3  add API sdr_thread_create(), sdr_thread_join(),
+//                   sdr_mutex_init(), sdr_mutex_lock(), sdr_mutex_unlock()
 //
 #include "pocket_sdr.h"
-#ifndef WIN32
+#ifdef WIN32
+#include <timeapi.h>
+#include <process.h>
+#else
 #include <time.h>
 #include <sys/time.h>
 #endif
@@ -22,8 +27,8 @@ static const char *lib_name = SDR_LIB_NAME;
 static const char *lib_ver = SDR_LIB_VER;
 
 //------------------------------------------------------------------------------
-//  Allocate memory. If no memory allocated, it exits the AP immediately with
-//  an error message.
+//  Allocate memory. memory is zero-cleared. If no memory allocated, it exits
+//  the AP immediately with an error message.
 //  
 //  args:
 //      size     (I)  memory size (bytes)
@@ -88,6 +93,10 @@ void sdr_get_time(double *t)
         t[3] = tt->tm_hour;
         t[4] = tt->tm_min;
         t[5] = tt->tm_sec + tv.tv_usec * 1E-6;
+    } else {
+        t[0] = 1970.0;
+        t[1] = t[2] = 1.0;
+        t[3] = t[4] = t[5] = 0.0;
     }
 #endif
 }
@@ -154,3 +163,123 @@ const char *sdr_get_ver(void)
     return lib_ver;
 }
 
+//------------------------------------------------------------------------------
+//  Create thread.
+//
+//  args:
+//      thread   (O)  pointer to thread
+//      func     (I)  thread function
+//      arg      (I)  pointer to function argument
+//
+//  return:
+//      status (1:OK,0:error)
+//
+#ifdef WIN32
+
+typedef struct {void *(*func)(void *); void *arg;} _ctx_t;
+
+static unsigned __stdcall _th_func(void *c)
+{
+    _ctx_t *ctx = (_ctx_t *)c;
+    ctx->func(ctx->arg); // call thread function
+    sdr_free(ctx);
+    return 0;
+}
+
+int sdr_thread_create(sdr_thread_t *thread, void *(func)(void *), void *arg)
+{
+    uintptr_t h;
+    unsigned tid;
+    _ctx_t *ctx = (_ctx_t *)sdr_malloc(sizeof(_ctx_t));
+    ctx->func = func;
+    ctx->arg = arg;
+    if (!(h = _beginthreadex(NULL, 0, _th_func, ctx, 0, &tid))) {
+        sdr_free(ctx);
+        return 0;
+    }
+    *thread = (HANDLE)h;
+    return 1;
+}
+
+#else
+
+int sdr_thread_create(sdr_thread_t *thread, void *(func)(void *), void *arg)
+{
+    return pthread_create(thread, NULL, func, arg) == 0;
+}
+
+#endif // WIN32
+
+//------------------------------------------------------------------------------
+//  Join thread. Blocked until the thread function exits.
+//
+//  args:
+//      thread   (I)  thread
+//
+//  return:
+//      none
+//
+void sdr_thread_join(sdr_thread_t thread)
+{
+#ifdef WIN32
+    if (!thread) return;
+    (void)WaitForSingleObject(thread, INFINITE);
+    CloseHandle(thread);
+#else
+    (void)pthread_join(thread, NULL);
+#endif
+}
+
+//------------------------------------------------------------------------------
+//  Initialize mutex.
+//
+//  args:
+//      mtx      (IO) mutex
+//
+//  return:
+//      none
+//
+void sdr_mutex_init(sdr_mutex_t *mtx)
+{
+#ifdef WIN32
+    if (mtx) InitializeSRWLock(mtx);
+#else
+    (void)pthread_mutex_init(mtx, NULL);
+#endif
+}
+
+//------------------------------------------------------------------------------
+//  Lock mutex.
+//
+//  args:
+//      mtx      (IO) mutex
+//
+//  return:
+//      none
+//
+void sdr_mutex_lock(sdr_mutex_t *mtx)
+{
+#ifdef WIN32
+    if (mtx) AcquireSRWLockExclusive(mtx);
+#else
+    (void)pthread_mutex_lock(mtx);
+#endif
+}
+
+//------------------------------------------------------------------------------
+//  Unlock mutex.
+//
+//  args:
+//      mtx      (IO) mutex
+//
+//  return:
+//      none
+//
+void sdr_mutex_unlock(sdr_mutex_t *mtx)
+{
+#ifdef WIN32
+    if (mtx) ReleaseSRWLockExclusive(mtx);
+#else
+    (void)pthread_mutex_unlock(mtx);
+#endif
+}
