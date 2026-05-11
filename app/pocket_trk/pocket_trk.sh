33c8bb7e177455723dbf3879e098195beb2b5574
ttaka <ttaka@yk.rim.or.jp>
2026-05-11 17:35:59 +0900

    Follow -fd option.

--- app/pocket_trk/pocket_trk.sh
+++ app/pocket_trk/pocket_trk.sh
@@ -21,15 +21,16 @@ esac
 OPT="-fo 1575.42 -sig L1CA -prn 1-32,194-199 -sig E1B -prn 1-36 -opt pocket_trk_default.conf"
 
 OUT="-nmea :10020 -rtcm :10021 -log :10022"
-#OUT="-nmea ./pocket_trk.nmea -rtcm ./pocket_trk.rtcm3 -log ./pocket_trk.log"
-LOG=/dev/null
-#LOG=./pocket_trk_dev.log
+#LOG=/dev/null
+LOG=./pocket_trk.log
 
 case $1 in
-    lime)  ./pocket_trk -driver lime     -fmt CS16 -f 32  -gain 30 -bw 8 $OPT $OUT 2> $LOG;;
-    usrp)  ./pocket_trk -driver uhd      -fmt CS16 -f 32  -gain 30 -bw 8 $OPT $OUT 2> $LOG;;
-    pluto) ./pocket_trk -driver plutosdr -fmt CS8  -f 6   -gain 30 -bw 4 $OPT $OUT 2> $LOG;;
-    rtl)   ./pocket_trk -driver rtlsdr   -fmt CS8  -f 2.4 -gain 30 -bw 2 $OPT $OUT 2> $LOG;;
-    *)     ./pocket_trk $OPT $OUT 2> $LOG;; # Pocket SDR FE
+    usrp)   ./pocket_trk -driver uhd      -fmt CS16 -f 48  -gain 30 -bw 8 $OPT $OUT 2> $LOG;;
+    lime)   ./pocket_trk -driver lime     -fmt CS16 -f 60  -gain 30 -bw 8 $OPT $OUT 2> $LOG;;
+    blade)  ./pocket_trk -driver bladerf  -fmt CS16 -f 40  -gain 30 -bw 4 $OPT $OUT 2> $LOG;;
+    pluto)  ./pocket_trk -driver plutosdr -fmt CS8  -f 6   -gain 30 -bw 4 -fd 20000 $OPT $OUT 2> $LOG;;
+    rtl)    ./pocket_trk -driver rtlsdr   -fmt CS8  -f 2.4 -gain 30 -bw 2 $OPT $OUT 2> $LOG;;
+    airspy) ./pocket_trk -driver airspy   -fmt CS16 -f 6   -gain 30 -bw 4 $OPT $OUT 2> $LOG;;
+    *)      ./pocket_trk $OPT $OUT 2> $LOG;; # Pocket SDR FE
 esac
 
