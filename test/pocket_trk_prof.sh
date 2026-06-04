1534d2cb5c848ea916f4c513725944fd2c07e94e
ttaka <ttaka@yk.rim.or.jp>
2026-06-04 15:10:51 +0900

    Moved to ./script or ./python.

--- test/pocket_trk_prof.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-#  profiling pocket_trk.py
-#
-
-python3 -m cProfile -s cumtime ../python/pocket_trk.py L1.bin -f 12 -fi 3 -sig L1CA -prn 193,194,196 -q > prof1.txt
-
-python3 -m cProfile -s cumtime ../python/pocket_trk.py L6.bin -f 12        -sig L6D -prn 193,194,196 -q > prof2.txt
-
